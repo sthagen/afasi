fe28880baec3ecac26abe5c730e42a6eda2910c2
Stefan Hagen <stefan@hagen.link>
2021-10-19 12:49:39 +0200

    Update test_afasi.py

--- tests/test_afasi.py
+++ tests/test_afasi.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pathlib
 
+import pytest
+
+import afasi.kysy as af
+
+
+def test_main_empty():
+    message = 'received wrong number of arguments'
+    with pytest.raises(UserWarning) as ex:
+        af.main([]) == 2
+        assert message in str(ex.value)
