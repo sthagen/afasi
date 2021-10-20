1744a7e8b3ba63fd8fbc719772b057bb9e9c7efc
Stefan Hagen <stefan@hagen.link>
2021-10-20 10:44:42 +0200

    Update test_afasi.py

--- tests/test_afasi.py
+++ tests/test_afasi.py
@@ -10,3 +10,14 @@ def test_main_empty():
     with pytest.raises(UserWarning) as ex:
         af.main([]) == 2
         assert message in str(ex.value)
+
+
+def test_main_unknown_command():
+    message = 'received wrong number of arguments'
+    with pytest.raises(UserWarning) as ex:
+        af.main(['fabulate', '', '', '', 'DRYRUN']) == 2
+        assert message in str(ex.value)
+
+
+def test_main_translate():
+    af.main(['translate', '', '', 'tests/fixtures/basic/fuzz.json', 'DRYRUN']) == 0
