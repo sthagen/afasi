0de9219872764b03a7a41495fb2b763eda7da5ed
Stefan Hagen <stefan@hagen.link>
2022-11-28 23:49:46 +0100

    Bumped impl version for release

--- afasi/__init__.py
+++ afasi/__init__.py
@@ -7,8 +7,8 @@ import pathlib
 from typing import List, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2022.11.26+parent.86239d31'
-# [[[end]]] (checksum: 617fbd33a47b7c832414735090fb7348)
+__version__ = '2022.11.28+parent.63423cd2'
+# [[[end]]] (checksum: d6cbb972a54f39fd72aca87bc1189801)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
